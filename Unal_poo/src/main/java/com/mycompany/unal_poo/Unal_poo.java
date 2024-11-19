697774489740e58c3bf9877c4533c51862476ce4
Daniel <Daniel@DESKTOP-60B4DUD>
2024-11-19 14:02:22 -0500

    First commit

---
+++ Unal_poo/src/main/java/com/mycompany/unal_poo/Unal_poo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unal_poo;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Unal_poo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
